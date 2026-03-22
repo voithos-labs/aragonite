77607987434363f81efb3b13e710eaeb080930ae
DanielZFLiu <daniel.zf.liu@gmail.com>
2026-03-22 00:02:15 -0400

    + (editor) BlockHost and BlockList components

---
+++ src/lib/components/BlockList.svelte
@@ -0,0 +1,25 @@
+<!-- src/lib/editor/components/BlockList.svelte -->
+<script lang="ts">
+    import type { MutableNode, BlockComponent } from '../editor-types';
+    import BlockHost from './BlockHost.svelte';
+
+    let { children, blockIds, blockRefs = $bindable([]) }:
+        {
+            children: MutableNode[];
+            blockIds: string[];
+            blockRefs?: (BlockComponent | undefined)[];
+        } = $props();
+</script>
+
+<div class="block-list">
+    {#each children as node, i (blockIds[i])}
+        <BlockHost {node} index={i} bind:ref={blockRefs[i]} />
+    {/each}
+</div>
+
+<style>
+    .block-list {
+        display: flex;
+        flex-direction: column;
+    }
+</style>
