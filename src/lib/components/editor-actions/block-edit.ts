b30f69e28a902105b7081c0d950c3fcab8564f45
DanielZFLiu <daniel.zf.liu@gmail.com>
2026-04-20 21:03:54 -0400

    ~ (editor) 0.5.5.2 prettier sweep on undo-controller + block-edit

--- src/lib/components/editor-actions/block-edit.ts
+++ src/lib/components/editor-actions/block-edit.ts
@@ -5,11 +5,7 @@
  * in one place.
  */
 
-import {
-	CURSOR_END,
-	type BlockEditActions,
-	type CstNode
-} from '../../contracts';
+import { CURSOR_END, type BlockEditActions, type CstNode } from '../../contracts';
 import { trimTrailingLineEnding, displayLength } from '../../core/lines';
 import {
 	splitNode as performSplit,
