2e9506110f7d5b60ebc781d0917b1b1ae4dbeee6
DanielZFLiu <daniel.zf.liu@gmail.com>
2026-06-22 11:54:49 -0400

    ~ (app) import Editor from the $lib/editor barrel (consumer + demo)

--- src/routes/test/editor/+page.svelte
+++ src/routes/test/editor/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import Editor from '$lib/editor/components/Editor.svelte';
+	import { Editor } from '$lib/editor';
 	import { parse } from '$lib/editor/core/parser';
 	import { serialize } from '$lib/editor/core/serializer';
 	import { SHOWCASE_CONTENT } from '$lib/editor/e2e/test-content';
