c5e74f87224414dfa4000bb69c1f177b385b2a1b
DanielZFLiu <daniel.zf.liu@gmail.com>
2026-07-30 20:33:45 -0400

    ! (config) unit svelte hmr takes vite-plugin-svelte 7's option name

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 import path from 'path';
 
 export default defineConfig({
-	plugins: [svelte({ hot: false })],
+	plugins: [svelte({ compilerOptions: { hmr: false } })],
 	resolve: {
 		alias: {
 			$lib: path.resolve('./src/lib')
